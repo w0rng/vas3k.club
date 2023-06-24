3c39dc2d7914bd526c642abf7e5793a74609b32c
w0rng <antonabramov2000@gmail.com>
2023-06-24 12:22:27 +0700

    ! fix db

--- club/settings.py
+++ club/settings.py
@@ -176,7 +176,6 @@ EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-print(EMAIL_HOST_PASSWORD, flush=True)
 EMAIL_USE_TLS = False
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "НеВсе.Клуб <club@notall.club>")
 
